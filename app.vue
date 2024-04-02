507650738aec998e593d6ea561aee29255fca0e1
nouenthary <tharynouen22@gmail.com>
2024-04-02 08:57:25 +0700

    add page home

--- app.vue
+++ app.vue
@@ -1,9 +1,16 @@
+import Home from './server/pages/home/Home.vue';
 <template>
   <div>
     <NuxtLink to="/"> Home </NuxtLink>
 
-    <NuxtLink to="/about"> About </NuxtLink>
+    <NuxtLink to="/home/home"> Home </NuxtLink>
 
-    <RouterView />
+    <NuxtLink to="/about/about"> About </NuxtLink>
+
+    <div>
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+    </div>
   </div>
 </template>
